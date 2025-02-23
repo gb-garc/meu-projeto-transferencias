e7283c7e98d955640a03eb81436132ed250ab805
gb-garc <165524994+gb-garc@users.noreply.github.com>
2025-02-23 10:24:50 +0100

    Corrigindo os.getenv

--- config/database.py
+++ config/database.py
@@ -5,21 +5,21 @@ from dotenv import load_dotenv
 # Carrega variáveis do arquivo .env (localmente)
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_NAME = os.getenv("DB_NAME")
+PGHOST = os.getenv('PGHOST')
+PGPORT = os.getenv('PGPORT')
+PGUSER = os.getenv('PGUSER')
+PGPASSWORD = os.getenv('PGPASSWORD')
+PGDATABASE = os.getenv('PGDATABASE')
 
 def get_connection():
     """
     Retorna uma conexão com o banco de dados PostgreSQL.
     """
     conn = psycopg2.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        user=DB_USER,
-        password=DB_PASS,
-        dbname=DB_NAME
+        host=PGHOST,
+        port=PGPORT,
+        user=PGUSER,
+        password=PGPASSWORD,
+        dbname=PGDATABASE
     )
     return conn
