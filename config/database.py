cb79dfa00b2b66deab919f87e16f0adabbd28170
gb-garc <165524994+gb-garc@users.noreply.github.com>
2025-02-23 12:06:05 +0100

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,25 +1,13 @@
 import os
 import psycopg2
-from dotenv import load_dotenv
 
-# Carrega variáveis do arquivo .env (localmente)
-load_dotenv()
+# Obtém a URL do banco de dados do Railway
+DATABASE_URL = os.getenv('DATABASE_URL')
 
-PGHOST = os.getenv('PGHOST')
-PGPORT = os.getenv('PGPORT')
-PGUSER = os.getenv('PGUSER')
-PGPASSWORD = os.getenv('PGPASSWORD')
-PGDATABASE = os.getenv('PGDATABASE')
+if not DATABASE_URL:
+    raise ValueError("Erro: A variável DATABASE_URL não está definida!")
 
 def get_connection():
-    """
-    Retorna uma conexão com o banco de dados PostgreSQL.
-    """
-    conn = psycopg2.connect(
-        host=PGHOST,
-        port=PGPORT,
-        user=PGUSER,
-        password=PGPASSWORD,
-        dbname=PGDATABASE
-    )
+    #Retorna uma conexão com o banco de dados PostgreSQL no Railway.
+    conn = psycopg2.connect(DATABASE_URL)
     return conn
